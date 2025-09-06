637aae9c95a61d2f0e4ca2c65cb52007de7576d9
Yan Zinchenko <12407564+YanZinchenko@users.noreply.github.com>
2025-09-06 10:20:23 +0200

    Declare variables

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"rock", "paper", "scissors"};
+        String playerChoice;
+        String computerChoice;
+        String playAgain = "yes";
+
+
+        scanner.close();
     }
 }
